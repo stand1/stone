f3259541327e7524b93d4d835df645347685afec
stone <568893757@qq.com>
2019-08-12 09:56:16 +0800

    'init'

--- store/src/components/HelloWorld.vue
+++ store/src/components/HelloWorld.vue
@@ -23,7 +23,8 @@ export default {
   data () {
     return {
       msg: 'Hello World',
-      listData: [1, 2, 3, 4, 5, 6, 7, 8]
+      listData: [1, 2, 3, 4, 5, 6, 7, 8],
+      dataMsg: 'Hello World'
     }
   },
   methods: {
